a65136964b3bcd12c4246d631a41adfff86999d4
AceXiamo <xiamo@qwq.link>
2024-03-23 21:44:20 +0800

    feat: add alias

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,16 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-iconify/plugin'
+      'react-native-iconify/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@': '.',
+          },
+        },
+      ],
     ],
   };
 };
