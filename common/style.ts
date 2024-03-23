0453d1f4a8c8e387d0554f6314036919ac588f94
AceXiamo <xiamo@qwq.link>
2024-03-23 21:44:43 +0800

    feat: add some style

--- common/style.ts
+++ common/style.ts
@@ -1,3 +1,13 @@
-export const globalFont = {
-  fontFamily: 'TsukuARdGothicStd-D'
-}
+export const globalFont: any  = {
+  fontFamily: 'TsukuARdGothicStd-D',
+};
+
+export const flexCol: any = {
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+export const flexRow: any = {
+  display: 'flex',
+  flexDirection: 'row',
+};
