a8174b5b4221ed2417c04a707f723528afc585c0
AceXiamo <xiamo@qwq.link>
2024-03-23 13:57:43 +0800

    chore: add some package

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
 import HomePage from './pages/home';
-import SettingsPage from './pages/settings';
+import MyPage from './pages/my';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Iconify } from 'react-native-iconify';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    // <NavigationContainer>
-      <Text>123123</Text>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Tabs/>
+    </NavigationContainer>
   );
 }
 
-// function Tabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Home" component={HomePage}/>
-//       <Tab.Screen name="Settings" component={SettingsPage}/>
-//     </Tab.Navigator>
-//   )
-// }
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Iconify icon="heroicons:home-solid" color={color} size={size}/>
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="My"
+        component={MyPage}
+        options={{
+          tabBarLabel: 'My',
+          tabBarIcon: ({ color, size }) => (
+            <Iconify icon="heroicons:user-solid" color={color} size={size}/>
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
