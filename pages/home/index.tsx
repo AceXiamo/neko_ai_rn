38fd007850b8c65225b8ed8ac14abe6655cf6fef
AceXiamo <xiamo@qwq.link>
2024-03-23 21:44:51 +0800

    feat: home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,12 +1,38 @@
 import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import Title from './components/Title'
-import Today from './components/Today'
+import Title from './components/Title';
+import Today from './components/Today';
+import Bottom from './components/Bottom';
+import { flexCol } from '@/common/style';
+import RecordCard from './components/RecordCard';
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <Title/>
-      <Today/>
+      <View
+        style={{
+          ...flexCol,
+          gap: 15,
+          backgroundColor: '#10b981',
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 5 },
+          shadowOpacity: 0.15,
+          paddingBottom: 15,
+          zIndex: 2,
+        }}
+      >
+        <Title />
+        <Today />
+        <Bottom />
+      </View>
+      <View
+        style={{
+          backgroundColor: '#E5E7EB',
+          flex: 1,
+          zIndex: 1,
+        }}
+      >
+        <RecordCard />
+      </View>
     </SafeAreaView>
   );
 }
@@ -15,8 +41,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#10b981',
     flex: 1,
-    display: "flex",
-    flexDirection: "column",
-    gap: 20
-  }
+    display: 'flex',
+    flexDirection: 'column',
+  },
 });
